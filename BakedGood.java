330a2d4bab508a26bedf9560834177edc412eceb
mahayawarkhan <mahayawar@gmail.com>
2024-01-16 10:30:39 -0500

    initial commit; wrote BakedGood class constructor

---
+++ BakedGood.java
@@ -0,0 +1,28 @@
+
+/**
+ * Represents our collection of baked items in a bakery
+ *
+ * @author (Maha Khan)
+ * @version (16-01-2024)
+ */
+public class BakedGood
+{
+    // instance variables - replace the example below with your own
+    private String name;
+    private String recipe;
+    private double price;
+    private int quantity;
+
+    /**
+     * Constructor for objects of class BakedGood
+     */
+    public BakedGood(String name, String recipe, double price, int quantity)
+    {
+        // initialise instance variables
+        this.name = name;
+        this.recipe = recipe;
+        this.price = price;
+        this.quantity = quantity;
+    
+    }
+}
